c4720e3ba509178d93b905e7598414c8768d81c3
Wisnu Andika <wisnuandika104@gmail.com>
2023-07-08 13:46:20 +0700

    implementasi database dan migrasi data

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
@@ -38,23 +39,13 @@ class PostController extends Controller
         $title = $request->input('title');
         $content = $request->input('content');
         
-        $posts = Storage::get('posts.txt');
-        $posts = explode("\n", $posts);
-
-        $new_post = [
-            count($posts)+1, 
-            $title,
-            $content,
-            date('Y-m-d H:i:s')
-        ];
-
-        $new_post = implode(',', $new_post);
-
-        array_push($posts, $new_post);
-
-        $posts = implode("\n", $posts);
-
-        Storage::write('posts.txt', $posts);
+        DB::table('posts')->insert([
+            'title' => $title,
+            'content' => $content,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        
         
         return redirect('posts');
     }

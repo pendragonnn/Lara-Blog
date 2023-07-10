5d13d68736a29c0481926e8938123b4c7196fe23
Wisnu Andika <wisnuandika104@gmail.com>
2023-07-10 14:34:01 +0700

    membuat relasi model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Comment;
 
 class Post extends Model
 {
@@ -13,5 +14,9 @@ class Post extends Model
 
     public function scopeActive($query) {
         return $query->where('active', true);
+    }
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
     }
 }
